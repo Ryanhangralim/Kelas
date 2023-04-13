61e3d80de150c04adb25bf9ad3e0d3bc0133cc68
Ryanhangralim <120095775+Ryanhangralim@users.noreply.github.com>
2023-04-13 10:01:07 +0800

    tugas4

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,14 +1,1 @@
-else
-            {
-                printf("\nData dalam stack: \n");
-                printf("No\tJudul\t\tPenerbit\tHalaman");
-                int j = 1;
-                for (int i = top; i >= 0; --i)
-                {
-                    printf("\n%d\t%s\t\t%s\t%d", j , buku[i].judul, buku[i].penerbit, buku[i].halaman);
-                    j=j+1;
-                }
-                printf("\n");
-                char ch;
-                printf("Apakah anda ingin kembali ke menu utama? (y/n)");
-                
+nt i = head; i<tails; i++
